1cf7cf9a0f87ab3c0731e92b32ccadf98e1cbfe2
Bruno  Corrêa <bruno.correa@estapar.com.br>
2025-10-10 21:34:59 -0300

    Teste

---
+++ arquivo_py/main.py
@@ -0,0 +1,20 @@
+import calculo
+
+print("ola mundo")
+print("programa do dobro")
+
+print("-" *50)
+
+x= float(input("digite um numero e vai retornar o dobro: " ))
+
+dobro = calculo.dobro(x)
+
+print(dobro)
+
+# teste
+
+y= input("digite um numero e vai retornar o triplo: " )
+
+triplo = calculo.triplo(y)
+
+print(triplo)
